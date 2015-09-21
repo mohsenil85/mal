d3f669f6c1012bbb107686963d10c386c3ce49d7
mohsenil85 <mohsenil85@gmail.com>
2015-09-21 16:58:24 -0700

    Start of step1_read_print

--- lisp/mal-lisp.lisp
+++ lisp/mal-lisp.lisp
@@ -4,24 +4,28 @@
         #:linedit
         #:cl-ppcre
         )
-  (:export #:repl))
+  (:export #:step0
+           #:step1))
 
 (in-package #:mal-lisp)
 
-;;step0
-
-(defun prompt ()
-  (format t "user> ")
-  (force-output))
+;;step0_repl
 
 (defun quit ()
   (fresh-line)
   (format t "bye~%")
   (sb-ext:exit))
 
+(defun prompt ()
+  (format t "user> ")
+  (force-output))
+
 (defun mal-read ()
+  (prompt)
   (handler-case
-      (linedit:linedit :prompt "user> ")
+      ;;(linedit:linedit :prompt "user> ") ; this would cause tests to fail
+      ;;even though it "werks"
+      (read-line)
     (end-of-file () (quit))))
 
 (defun mal-eval (arg)
@@ -30,9 +34,24 @@
 (defun mal-print (arg)
   (format t "~A~%" arg))
 
-(defun repl (args)
-  (declare (ignore args))
+(defun repl ()
   (loop
      (mal-print
       (mal-eval
        (mal-read)))))
+
+(defun step0 (args)
+  (declare (ignore args))
+  (repl))
+
+
+;;step1_read_print
+
+;(defun make-reader (tokens)
+  ;(lambda (cmd) (case cm))
+  ;)
+
+(defun step1 (args)
+  (declare (ignore args))
+  (repl)
+  )
